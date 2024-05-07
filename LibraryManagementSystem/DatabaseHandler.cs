2e92fb7bc5e7722f8b767b3a9426fe0b6aa80592
ibrakadabraz <https://ibrakadabraz@github.com>
2024-05-08 02:05:25 +0300

    first commit

---
+++ LibraryManagementSystem/DatabaseHandler.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem
+{
+    public static class DatabaseHandler
+    {
+        private static String ServerName = @"SERVER BURAYA GELECEK";
+
+        private static SqlConnection sqlConnection;
+
+        public static SqlConnection GetNewConnection()
+        {
+            try
+            {
+                //Data Source=SERVER ADRESI;Initial Catalog=LIBRARYMS;Integrated Security=True
+                sqlConnection = new SqlConnection("Data Source='" + ServerName + "'; Initial Catalog=LIBRARYMS;Integrated Security=True");
+            }
+            catch (SqlException e)
+            {
+                System.Windows.Forms.MessageBox.Show("Error: " + e.ToString());
+            }
+            return sqlConnection;
+        }
+
+        public static String GetServerName()
+        {
+            return ServerName;
+        }
+
+    }
+}
